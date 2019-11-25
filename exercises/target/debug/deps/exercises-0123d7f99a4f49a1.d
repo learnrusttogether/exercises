0f6180c8b371f81ef9a389ac23bf0df498101b6a
ilya <ilya@ilyas-MacBook-Air.local>
2019-11-25 09:10:45 +0300

    exercises folder

---
+++ exercises/target/debug/deps/exercises-0123d7f99a4f49a1.d
@@ -0,0 +1,7 @@
+/Users/ilya/Documents/GitHub/exercises/exercises/target/debug/deps/exercises-0123d7f99a4f49a1.rmeta: src/lib.rs
+
+/Users/ilya/Documents/GitHub/exercises/exercises/target/debug/deps/libexercises-0123d7f99a4f49a1.rlib: src/lib.rs
+
+/Users/ilya/Documents/GitHub/exercises/exercises/target/debug/deps/exercises-0123d7f99a4f49a1.d: src/lib.rs
+
+src/lib.rs:
---
+++ exercises/target/debug/exercises-92f8409e9c397d71.d
@@ -0,0 +1,1 @@
+/Users/ilya/Documents/GitHub/exercises/exercises/target/debug/exercises-92f8409e9c397d71: /Users/ilya/Documents/GitHub/exercises/exercises/src/lib.rs
