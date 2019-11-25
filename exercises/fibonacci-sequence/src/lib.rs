07e2d2eb60676987825db95ecbe5d1d247fbf3f8
ilya <ilya@ilyas-MacBook-Air.local>
2019-11-25 23:12:02 +0300

    fibonacci-sequence

--- exercises/fibonacci-sequence/src/lib.rs
+++ exercises/fibonacci-sequence/src/lib.rs
@@ -1,3 +1,20 @@
-pub fn fibonacci(_n: i32) -> Option<i32> {
+pub fn fibonacci(n: i32) -> Option<i32> {
+    let mut a: i32 = 1;
+    let mut b: i32 = 1;
+    let mut c: i32;
+
+    if n == 0 {
+        return Some(0);
+    } else if n == 1 {
+        return Some(1);
+    } else if n > 1 {
+        for _i in 2..n {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return Some(b);
+    }
+
     None
 }
